865943e148dc291b9554df42c97395b07c2cad4b
Nougaga <zmni123@naver.com>
2020-12-24 22:38:57 +0900

    Add app(pred), base templates
    - pred : 예측결과 출력을 담당할 app
    - bootstrap templates 이용

--- hostingTest/urls.py
+++ hostingTest/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.default_view),
+    path('pred/', include('pred.urls')),
 ]

7094638d5cba0a9804328b1708cee604f5971d0e
Nougaga <54338876+Nougaga@users.noreply.github.com>
2021-09-12 15:13:49 +0900

    Initial commit

---
+++ src/main/java/nougaga/ItemInfoProvider/ItemInfoProviderApplication.java
@@ -0,0 +1,13 @@
+package nougaga.ItemInfoProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemInfoProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemInfoProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nougaga/ItemInfoProvider/ItemInfoProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package nougaga.ItemInfoProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemInfoProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
